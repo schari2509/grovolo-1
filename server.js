759cdc06624cf63bfddc6486f9edc90ca22d161e
Tejas Lotlikar <mrt3j45@gmail.com>
2020-07-26 21:09:56 +0530

    fixed typo in built -> build

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/auth',require('./routes/api/auth'))
 
 //serve static assets in production
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/built'))
+    app.use(express.static('client/build'))
 
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','built','index.html'))
